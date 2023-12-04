6412ced309fefd4471ee24d37e9fd6e813af9c20
Ananiya Belew <ananiya@arifpay.org>
2023-12-04 07:55:29 +0300

    odoo arifpay

---
+++ Custom-addons/Arifpay/__manifest__.py
@@ -0,0 +1,16 @@
+{
+
+    'name': 'Arifpay Gateway',
+    'version': '1.0.0.0.0',
+    'summary': 'Tool used to integrate with arifpays gateway ',
+    'sequence': -50,
+    'description': """
+    Long Description of your modules""",
+    'author': 'Ananiya',
+    'company': 'Arifpay',
+    'website': 'developer.arifpay.org',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'application': True,
+}
